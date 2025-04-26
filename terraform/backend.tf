634e352571fecb62c5943a4b6288b4207dec38e3
GowthamS-7 <92256560+GowthamS-7@users.noreply.github.com>
2025-04-26 15:42:36 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "hcl-state-bucket"
+    key            = "terraform-eks/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "hcl-eks-db"
+  }
+}
