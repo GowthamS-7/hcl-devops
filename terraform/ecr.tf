aae31581a73935a88506da7ec6357945c16fcc5f
GowthamS-7 <gowthamsenthilkumar37@gmail.com>
2025-04-26 19:06:29 +0530

    appoitment testing

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -14,3 +14,19 @@ module "ecr" {
   scan_images_on_push = true
   use_fullname = true
 }
+module "ecr" {
+  source  = "cloudposse/ecr/aws"
+  version = "0.34.0"
+  delimiter = "-"
+  id_length_limit = 12
+  label_key_case = "title"
+  label_value_case = "lower"
+  name = "ssm-bastion-1"
+  namespace = "terraform-ecr"
+  enable_lifecycle_policy = true
+  image_names = ["hcl-devops-appointment"]
+  image_tag_mutability = "IMMUTABLE"
+  max_image_count = 500
+  scan_images_on_push = true
+  use_fullname = true
+}
